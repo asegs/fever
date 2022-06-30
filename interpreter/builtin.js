be769fe1cc57994292b41239acc315abd8b04e72
asegs <ajs2290@g.rit.edu>
2022-06-30 14:11:18 -0400

    really sketchy user defined functions

---
+++ interpreter/builtin.js
@@ -0,0 +1,29 @@
+module.exports = {
+    functions: {
+        "+": {
+            arity: [[0,0],[0]],
+            operation: (a,b) => a+b,
+            help: "Adds two numbers.",
+            generated: false
+        },
+        "-": {
+            arity: [[0,0],[0]],
+            operation: (a,b) => a-b,
+            help: "Subtracts two numbers.",
+            generated: false
+        },
+        "*": {
+            arity: [[0,0],[0]],
+            operation: (a,b) => a*b,
+            help: "Multiplies two numbers.",
+            generated: false
+        },
+        "/": {
+            arity: [[0,0],[0]],
+            operation: (a,b) => a/b,
+            help: "Divides two numbers.",
+            generated: false
+        }
+
+    }
+}
--- interpreter/interpreter.js
+++ interpreter/interpreter.js
@@ -1,3 +1,5 @@
+const builtin = require('./builtin');
+
 const tokenize = (line) => {
 	for (let i = 0 ; i < line.length ; i ++ ) {
 		//Ignore and exclude spaces.
@@ -6,14 +8,6 @@ const tokenize = (line) => {
 		}
 	}
 	return ["_",line]
-}
-
-const functions = {
-	add: {
-		arity: [[0,0],[0]],
-		operation: (a,b) => a+b,
-		help: "Adds two numbers."
-	}
 }
 
 function* argGenerator (expression) {
@@ -35,10 +29,14 @@ const functor = (gen, vars) => {
 	if (isNumeric(arg)) {
 		arg = parseInt(arg);
 	}
-	if (arg in functions) {
-		const func = functions[arg];
+	if (arg in builtin.functions) {
+		const func = builtin.functions[arg];
 		const spreadables = func.arity[0].map(_ => functor(gen, vars));
-		return func.operation(...spreadables);
+		if (func.generated) {
+            return func.operation(spreadables);
+        }else {
+            return func.operation(...spreadables);
+        }
 	} else if (arg in vars) {
 		return vars[arg];
 	} else {
@@ -46,15 +44,45 @@ const functor = (gen, vars) => {
 	}
 }
 
+const isFunctionDef = (token) => {
+    return token.includes(" ");
+}
+
+const generateFunction = (token, action, vars) => {
+    const assignArgs = token.split(" ");
+    const funcName = assignArgs[0];
+    const args = assignArgs.slice(1);
+     builtin.functions[funcName] = {
+         arity: [args.map(_=>0),[0]],
+         operation: (supplied) => {
+             const suppliedMap = {};
+             for (let i = 0 ; i < args.length ; i ++ ) {
+                 suppliedMap[args[i]] = supplied[i];
+             }
+             return interpretExpression(action, {...vars, ...suppliedMap});
+         },
+         generated: true
+     }
+}
+
+const interpretExpression = (expr, vars) => {
+    const gen = argGenerator(expr, vars);
+    return functor(gen, vars);
+}
+
 const interpretLine = (line,vars) => {
-	const tokens = tokenize(line);
-	const gen = argGenerator(tokens[1])
-	const result = functor(gen,vars);
-	if (tokens[0] !== '_') {
-		vars[tokens[0]] = result;
-	}
-	console.log(result);
-	return vars;
+	const tokens = tokenize(line).map(token=>token.trim());
+	if (isFunctionDef(tokens[0])) {
+	    generateFunction(tokens[0], tokens[1], vars);
+	    return vars;
+    } else {
+        const result = interpretExpression(tokens[1], vars);
+        if (tokens[0] !== '_') {
+            vars[tokens[0]] = result;
+        }
+        console.log(result);
+        return vars;
+    }
 }
 
 const interpretBlock = (text) => {
@@ -64,3 +92,5 @@ const interpretBlock = (text) => {
 		vars = interpretLine(line,vars);
 	}
 }
+
+console.log(interpretBlock("add a b = + a b\nc = add 3 2"))
