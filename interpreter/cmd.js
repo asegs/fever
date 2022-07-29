ecce0aa6a67978e5f84e4b43cc333e11987154ee
Aaron Segal <ajs2290@g.rit.edu>
2022-07-29 15:16:50 -0400

    File sync processing

--- interpreter/cmd.js
+++ interpreter/cmd.js
@@ -26,6 +26,19 @@ const test = require('./test');
 const vars = interpreter.createVars();
 
 try {
+
+    async function streamToString(stream) {
+        // lets have a ReadableStream as a stream variable
+        const chunks = [];
+
+        for await (const chunk of stream) {
+            chunks.push(Buffer.from(chunk));
+        }
+
+        return Buffer.concat(chunks).toString("utf-8");
+    }
+
+
     let inputFile = argv._[0];
     if (inputFile) { // Handle a file path as the zeroth argument
         const inputPath = path.resolve(inputFile);
@@ -33,11 +46,8 @@ try {
             console.error("No such input file: " + inputPath);
             process.exit(1);
         }
-        const file = fs.createReadStream(inputPath);
-        const rl = readline.createInterface({input: file});
-        rl.on('line', (line) => {
-            interpreter.interpretLine(line, vars);
-        })
+        const file = fs.readFileSync(inputPath,'utf8');
+        file.split("\n").forEach(line => interpreter.interpretLine(line, vars));
     }
 
     if (argv.t) {
